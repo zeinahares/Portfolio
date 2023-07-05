83b467c79471c2566efa8ec6e54140f81e5f4d6a
zeinahares <zeinahares@gmail.com>
2023-07-05 22:54:50 +1000

    populated resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,10 +6,58 @@ export default function Resume() {
   return (
     <div>
       <h1>MY Skillzzz</h1>
-      <p>
-{/* list of skills */}
-      {/* downloadable resume link */}
+      <p>< a href="/resume.pdf" target="_blank" download>Click Here to Download My Resume</a>
       </p>
+      <h2> my list of skills</h2>
+
+      <h3>Front End</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+        <li>Handlebars</li>
+        <li>Progressive Web Apps</li>
+        <li>Webpack</li>
+      </ul>
+
+      <h3>Back End</h3>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
+
+      <h3>Dev Tools</h3>
+      <ul>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Heroku</li>
+        <li>Terminal</li>
+        <li>npm</li>
+        <li>Jest</li>
+        <li>Chrome DevTools</li>
+        <li>Insomnia</li>
+        <li>MySQL Workbench</li>
+        <li>MongoDB Compass</li>
+      </ul>
+
+      <h3>Frameworks</h3>
+      <ul>
+        <li>Object Oriented Programming</li>
+        <li>Object Relational Mapping</li>
+        <li>Model View Controller</li>
+        <li>MERN Stack</li>
+        <li>Progressive Web Applications</li>
+        <li>Single Page Applications</li>
+        <li>Test Driven Development</li>
+      </ul>
     </div>
   );
 }
