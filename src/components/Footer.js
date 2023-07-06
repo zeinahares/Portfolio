95ca859661225d86d4459eb310ed2a579d6a59de
zeinahares <zeinahares@gmail.com>
2023-07-06 19:15:59 +1000

    still debugging images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer'>
       {/* three logos, github, linked in, and a 3rd */}
-      <a href="https://www.instagram.com/zeinadoesart/" rel="noreferrer" target="_blank"> <img src=".public/images/insta.png" alt="insta logo" /></a>
+      <a href="https://www.instagram.com/zeinadoesart/" rel="noreferrer" target="_blank"> <img src="./images/insta.png" alt="insta logo" /></a>
       <a href="https://github.com/zeinahares" rel="noreferrer" target="_blank"> <img src="../../../public/images/github-mark.png" alt="github logo" /></a>
       <a href="https://www.linkedin.com/in/zeinahares/" rel="noreferrer" target="_blank"> <img src="../../../public/images/LI-In-Bug.png" alt="linked in logo" /></a>
     </div>
