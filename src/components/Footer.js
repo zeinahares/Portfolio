784bde4c371a759cf5731e105f95e0a3f7dae40a
zeinahares <zeinahares@gmail.com>
2023-07-06 14:59:53 +1000

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function Footer({ currentPage, handlePageChange }) {
   return (
- <div>
-  {/* three logos, github, linked in, and a 3rd */}
-  <p>hi</p>
- </div>
+    <div className='footer'>
+      {/* three logos, github, linked in, and a 3rd */}
+      <a href="https://www.instagram.com/zeinadoesart/"> <img src="/images/insta.png" alt="insta logo" /></a>
+      <a href="https://github.com/zeinahares"> <img src="/images/github-mark.png" alt="github logo" /></a>
+      <a href="https://www.linkedin.com/in/zeinahares/"> <img src="/images/LI-In-Bug.png" alt="linked in logo" /></a>
+    </div>
   );
 }
 
