df1e806e1da0cb0f18c7cc32a6fe990fcdde05e2
zeinahares <zeinahares@gmail.com>
2023-07-06 14:44:02 +1000

    about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import '../../styles/about.css'
 
 export default function About() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <div className='about'>
+      <div className='aboutTitle'>
+        <h1>About Me</h1>
+        <img id='headshot' src="/images/zeinaheadshot.png" alt="Zeina Headshot" />
+      </div>
+      <div className='aboutContent'>
+
+        <p>
+          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        </p>
+      </div>
+
+    </div >
   );
 }
