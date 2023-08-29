54392a2b3959cb7654fdb535d125d372edf01f1c
zeinahares <zeinahares@gmail.com>
2023-08-29 12:44:07 +1000

    added description and more projects

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,12 +11,7 @@ export default function About() {
       <div className='aboutContent'>
 
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Full Stack Web Developer currently completing a Bachelors of Computer Science at UNSW. Acquired recently a certificate in Full Stack Web Development from the University of Sydney’s Coding Boot Camp. Possessing a background in Media Screen and Sound Production, offering distinct viewpoints in project creation and management, as well as in designing end-user interaction for websites and software platforms. Demonstrating innovation in problem-solving and a dedicated interest in app development, with a specialized emphasis on backend design and management, as well as mobile-first design and development. Fostering creativity, collaboration within teams, and seamlessly steering projects from conceptualization to realization.
         </p>
       </div>
 
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,6 +3,93 @@ import '../../styles/portfolio.css';
 
 export default function Portfolio() {
 
+  const projects = [
+    {
+      title: 'Qwizard',
+      description: 'React/GraphQL/Mongoose/Express/Antd/Node/JS',
+      image: './images/qwizard.png',
+      github: 'https://github.com/zeinahares/Group-Project-3-Qwizard',
+      deployed: 'https://qwizard-c649b0755527.herokuapp.com/'
+    },
+    {
+      title: 'Pawesome Care',
+      description: 'HandleBars/CSS/JS/Sequelize/NodeJS/ExpressJS',
+      image: './images/pawesome-screenshot5.png',
+      github: 'https://github.com/zeinahares/PawsomeCare',
+      deployed: 'https://pawesomecare1.herokuapp.com/'
+    },
+    {
+      title: 'JATE',
+      description: 'HTML/CSS/JS/NodeJS/ExpressJS/npm/PWA/Webpack',
+      image: './images/jate.png',
+      github: 'https://github.com/zeinahares/Coding-Text-Editor',
+      deployed: 'https://jate-code-9aec63e172ae.herokuapp.com/'
+    },
+    {
+      title: 'The Tech Blog',
+      description: 'HandleBars/CSS/JS/Sequalize/NodeJS/ExpressJS',
+      image: './images/screenshot1.png',
+      github: 'https://github.com/zeinahares/Developer-Blog',
+      deployed: 'https://developer-tech-blog.herokuapp.com/'
+    },
+    {
+      title: 'Note Taker Databse',
+      description: 'HTML/CSS/JS/NodeJS/ExpressJS',
+      image: './images/full-note.png',
+      github: 'https://github.com/zeinahares/Note-Taker-Database',
+      deployed: 'https://note-taker-database.herokuapp.com/'
+    },
+    {
+      title: 'Google Book Search',
+      description: 'HTML/CSS/JS/API/React',
+      image: './images/screenshot.png',
+      github: 'https://github.com/zeinahares/Google-Books',
+      deployed: 'https://googlebookssearch-89b1230c1db2.herokuapp.com/'
+    },
+    {
+      title: 'CineFile',
+      description: 'HTML/CSS/JS/JQuery/API',
+      image: './images/cinefile.png',
+      github: 'https://github.com/zeinahares/Group-Project-1-CineFile',
+      deployed: 'https://zeinahares.github.io/Group-Project-1-CineFile/'
+    },
+    {
+      title: 'Weather Dashboard',
+      description: 'HTML/CSS/JS/API',
+      image: './images/weather.jpg',
+      github: 'https://github.com/zeinahares/Weekly-Weather',
+      deployed: 'https://zeinahares.github.io/Weekly-Weather/'
+    },
+    {
+      title: 'Password Generator',
+      description: 'HTML/CSS/JS',
+      image: './images/password.jpg',
+      github: 'https://github.com/zeinahares/Password-Generator',
+      deployed: 'https://zeinahares.github.io/Password-Generator/'
+    },
+    {
+      title: 'Work Day Scheduler',
+      description: 'HTML/CSS/JS',
+      image: './images/workday.jpg',
+      github: 'https://github.com/zeinahares/Workday-To-Do-List',
+      deployed: 'https://zeinahares.github.io/Workday-To-Do-List/'
+    },
+    {
+      title: 'Code Quiz',
+      description: 'HTML/CSS/JS',
+      image: './images/CodeQuiz2.gif',
+      github: 'https://github.com/zeinahares/Z-s-Code-Quiz',
+      deployed: 'https://zeinahares.github.io/Z-s-Code-Quiz/'
+    },
+    {
+      title: '1st Portfolio Website',
+      description: 'HTML/CSS/JS',
+      image: './images/website_long.png',
+      github: 'https://github.com/zeinahares/zeina_portfolio',
+      deployed: 'https://zeinahares.github.io/zeina_portfolio/'
+    },
+  ];
+
   return (
     <div class="page">
       <div class="header">
@@ -14,73 +101,18 @@ export default function Portfolio() {
       </div>
 
       <div class="section_content">
-
-      <a href="https://qwizard-c649b0755527.herokuapp.com/" rel="noreferrer" target="_blank" class="work_card">
-          <img src="./images/qwizard.png" alt="Project Snapshot of a mandebrot set" />
+        {projects.map((project) => (
+          <a href={project.deployed} rel="noreferrer" target="_blank" class="work_card">
+            <img src={project.image} alt={project.title} />
             <div class="title_card">
               <div class="title_content">
-                <h3>Qwizard</h3>
-                <p>React/GraphQL/Mongoose/Express/Antd/Node/JS</p>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
               </div>
-              <a href='https://github.com/zeinahares/Group-Project-3' rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
+              <a href={project.github} rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
             </div>
-        </a>
-
-        <a href="https://pawesomecare1.herokuapp.com/" rel="noreferrer" target="_blank" class="work_card">
-          <img src="./images/pawesome-screenshot5.png" alt="Project Snapshot of a mandebrot set" />
-            <div class="title_card">
-              <div class="title_content">
-                <h3>Pawesome Care</h3>
-                <p>HandleBars/CSS/JS/Sequalize/NodeJS/ExpressJS</p>
-              </div>
-              <a href='https://github.com/zeinahares/PawsomeCare' rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
-            </div>
-        </a>
-
-        <a href="https://jate-code-9aec63e172ae.herokuapp.com/" rel="noreferrer" target="_blank" class="work_card">
-          <img src="./images/jate.png" alt="jate snapshot" />
-            <div class="title_card">
-              <div class="title_content">
-                <h3>JATE</h3>
-                <p>HTML/CSS/JS/NodeJS/ExpressJS/npm/PWA/Webpack</p>
-              </div>
-              <a href='https://github.com/zeinahares/Coding-Text-Editor' rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
-            </div>
-        </a>
-
-        <a href="https://developer-tech-blog.herokuapp.com/8" rel="noreferrer" target="_blank" class="work_card">
-          <img src="./images/screenshot1.png" alt="Project Snapshot of a mandebrot set" />
-            <div class="title_card">
-              <div class="title_content">
-                <h3>The Tech Blog</h3>
-                <p>HandleBars/CSS/JS/Sequalize/NodeJS/ExpressJS</p>
-              </div>
-              <a href='https://github.com/zeinahares/Developer-Blog' rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
-            </div>
-        </a>
- 
-        <a href="https://note-taker-database.herokuapp.com/"  rel="noreferrer" target="_blank" class="work_card">
-          <img src="./images/full-note.png" alt="Project Snapshot of a mandebrot set" />
-            <div class="title_card">
-              <div class="title_content">
-                <h3>Note Taker Databse</h3>
-                <p>HTML/CSS/JS/NodeJS/ExpressJS</p>
-              </div>
-              <a href='https://github.com/zeinahares/Note-Taker-Database' rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
-            </div>
-        </a>
- 
-        <a href="https://zeinahares.github.io/Group-Project-1-CineFile/" rel="noreferrer" target="_blank" class="work_card">
-          <img src="./images/cinefile.png" alt="Project Snapshot of a mandebrot set" />
-            <div class="title_card">
-              <div class="title_content">
-                <h3>CineFile</h3>
-                <p>HTML/CSS/JS/API</p>
-              </div>
-              <a href='https://github.com/zeinahares/Group-Project-1-CineFile' rel="noreferrer" target="_blank"><img src="./images/github-mark.png" alt="Github Logo" class="logo" /></a>
-            </div>
-        </a>
- 
+          </a>
+        ))}
       </div>
     </div>
   );
